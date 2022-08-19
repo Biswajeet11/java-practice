0dce409bc2bef5b85a59fb36738ea1d39b58377a
Biswajeet  Gope <biswajeetgope@Biswajeets-MacBook-Air.local>
2022-08-19 20:29:18 +0530

    reduce eaxmple

--- src/java8/streams/StreamReduceExample.java
+++ src/java8/streams/StreamReduceExample.java
@@ -1,12 +1,22 @@
 package java8.streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamReduceExample {
+
+    public static int performMultiplication(List<Integer> integerList){
+        return integerList.stream()
+                .reduce(1,(a,b)-> a*b);
+    }
 
 
 
 
 
     public static void main(String[] args) {
+
+        List<Integer> integers = Arrays.asList(1,3,5,7);
 
     }
 
